28cf1de2f23a18759346237ec8f41932a6238055
Amjad Albanna <aalbanna7200@conestogac.on.ca>
2026-04-02 13:50:26 -0400

    Profile screen and navigation bar

--- projectapplication/lib/main.dart
+++ projectapplication/lib/main.dart
@@ -1,18 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:projectapplication/screens/login_screen.dart';
+import 'screens/cart_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/wishlist_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'URBNOVA',
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  List<Widget> _screens = [
+    HomeScreen(),
+    SearchScreen(),
+    WishlistScreen(),
+    ProfileScreen(),
+    CartScreen(),
+  ];
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
---
+++ projectapplication/lib/screens/wishlist_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WishlistScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Wishlist')),
+      body: Center(child: Text('Wishlist Screen')),
+    );
+  }
+}
